8016b846af451e8d67fabb2fbbfcdc20d5357852
cw <fromnowwon@gmail.com>
2023-06-03 15:04:47 +0900

    ArrayList_06_두 수의 합

---
+++ ArrayList/_06_두 수의 합.py
@@ -0,0 +1,16 @@
+def solution(nums, target):
+    nums = sorted(nums)
+    answer = [0] * 2 # 답이 없으면 [0, 0] 출력이기 때문에 이렇게 초기화 함
+    n = len(nums)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] == target:
+                return [nums[i], nums[j]]
+    return answer
+
+print(solution([7, 9, 2, 13, 3, 15, 8, 11], 12))
+print(solution([21, 12, 30, 15, 6, 2, 9, 19, 14], 24))
+print(solution([12, 18, 5, 8, 21, 27, 22, 25, 16, 2], 28))
+print(solution([11, 17, 6, 8, 21, 9, 19, 12, 25, 16, 2], 26))
+print(solution([7, 5, 12, -9, -12, 22, -30, -35, -21], -14))
+print(solution([7, 5, 12, 20], 15))

2310d0495e19f5e899f697766ccd3cdf962904c1
cw <fromnowwon@gmail.com>
2023-06-22 21:01:21 +0900

    GreedyAlgorithm_02_최대 사과의 개수

---
+++ 05_GreedyAlgorithm/_02_최대 사과의 개수.py
@@ -0,0 +1,19 @@
+# 한 박스당 가장 많이 담긴 박스부터 싣는다.
+def solution(box, limit):
+    answer = 0
+    box.sort(key=lambda v: -v[1]) # 2번째 요소 내림차순
+    print(box)
+    for x in box:
+        cnt = min(limit, x[0])
+        answer += (cnt * x[1])
+        limit -= cnt
+        if limit == 0:
+            break
+
+    return answer
+
+print(solution([[2, 20], [2, 10], [3, 15], [2, 30]], 5))
+print(solution([[1, 50], [2, 20], [3, 30], [2, 31], [5, 25]], 10))
+print(solution([[3, 40], [5, 20], [5, 70], [1, 80], [5, 30], [3, 35]], 15))
+print(solution([[2, 70], [5, 100], [3, 90], [1, 95]], 8))
+print(solution([[80, 20], [50, 10], [70, 15], [70, 30], [80, 70], [90, 88], [70, 75]], 500))

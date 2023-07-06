ffbf25925098fedbaf5f7db142c89017a2dbfcbe
cw <fromnowwon@gmail.com>
2023-07-06 22:05:01 +0900

    07_Stack_Queue_01_올바른 괄호

---
+++ 07_Stack_Queue/_01_올바른 괄호.py
@@ -0,0 +1,12 @@
+# Stack 사용
+def solution(s):
+    answer = "YES"
+    stack = []
+
+    return answer
+
+print(solution("((()))()"))
+print(solution("(()(()"))
+print(solution("()())"))
+print(solution("())("))
+print(solution("((())))()("))
